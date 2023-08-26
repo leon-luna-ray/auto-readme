e9d9e72192ba6baf0d381a78555c72f4455d5588
Ray Luna <leon.luna.ray@gmail.com>
2023-08-26 11:34:33 -0700

    add line reveal animation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="h-screen blue-border">
-    <div class="p-[1rem] m-[2rem] red-border animate-wiggle">Readme Generator</div>
+  <div class="h-screen blue-border hi">
+    <div class="p-[1rem] m-[2rem] red-border">Readme Generator</div>
+    <div class="terminal font-mono border border-gray-300 p-4">
+      <div v-for="(line, index) in lines" :key="index" class="animate-line-reveal relative">
+        {{ line }}
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+import { ref } from 'vue';
 
+const lines = ref([
+  "Welcome to the Vue Terminal Component!",
+  "This is line 2.",
+  "And this is the third line.",
+])
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,55 @@ export default {
   theme: {
     extend: {
       animation: {
+        none: 'none',
+        bounce: 'bounce 1s infinite',
+        line: 'line 5s cubic-bezier(0.07, 0.15, 0.6, .75) infinite',
+        'line-reveal': 'line-reveal 5s cubic-bezier(0.07, 0.15, 0.6, 0.75) infinite',
+        ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+        pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        spin: 'spin 1s linear infinite',
         'spin-slow': 'spin 3s linear infinite',
         wiggle: 'wiggle 4s ease-in-out infinite',
       },
       keyframes: {
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateY(-25%)',
+            animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+          },
+          '50%': {
+            transform: 'none',
+            animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+          },
+        },
+        line: {
+          '0%, 75%, 100%': { top: '31px' },
+          '15%, 60%': { top: 0 },
+        },
+        'line-reveal': {
+          '0%, 75%, 100%': {
+            clipPath: 'polygon(0 0, 100% 0, 100% 0, 0 0)',
+          },
+          '15%, 60%': {
+            clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+          },
+        },
+        ping: {
+          '75%, 100%': {
+            transform: 'scale(2)',
+            opacity: '0',
+          },
+        },
+        pulse: {
+          '50%': {
+            opacity: '.5',
+          },
+        },
+        spin: {
+          to: {
+            transform: 'rotate(360deg)',
+          },
+        },
         wiggle: {
           '0%, 100%': { transform: 'rotate(-3deg)' },
           '50%': { transform: 'rotate(3deg)' },
