fe855fdaa6416982f5980cb2133f167857328a2c
Ray Luna <leon.luna.ray@gmail.com>
2023-09-09 18:16:55 -0700

    update links and description

--- src/lib/content.js
+++ src/lib/content.js
@@ -9,16 +9,19 @@ export const content = {
       type: 'text',
       name: 'title',
       prompt: 'What is the project title?',
-      template: (title) => {
-        return `# ${title}`;
+      template: (value) => {
+return `# ${value}`;
       },
     },
     {
       type: 'text',
       name: 'description',
       prompt: 'A short description of the project',
-      template: (title) => {
-        return `# ${title}`;
+      template: (value) => {
+        return `
+## Description
+${value}
+        `;
       },
     },
     // {
@@ -39,42 +42,49 @@ export const content = {
       type: 'text',
       name: 'appUrl',
       prompt: 'What is the project URL?',
-      template: (title) => {
-        return `# ${title}`;
+      template: (value) => {
+        return `
+## Link
+${value}
+            `;
       },
     },
-    {
-      type: 'text',
-      name: 'repoUrl',
-      prompt: 'What is the project repo URL?',
-      template: (title) => {
-        return `# ${title}`;
-      },
-    },
+    // Todo need to move to store to create template
+    // {
+    //   type: 'text',
+    //   name: 'repoUrl',
+    //   prompt: 'What is the project repo URL?',
+    //   template: (value) => {
+    //     return `# ${value}`;
+    //   },
+    // },
     {
       type: 'text',
       name: 'imageUrl',
       prompt: 'What is the project image URL or file path?',
-      template: (title) => {
-        return `# ${title}`;
+      template: (value) => {
+        return `
+# Image
+![Screenshot](${value})
+        `;
       },
     },
-    {
-      type: 'text',
-      name: 'bugs',
-      prompt: 'A despription of any known bugs',
-      template: (title) => {
-        return `# ${title}`;
-      },
-    },
-    {
-      type: 'text',
-      name: 'nextSteps',
-      prompt: 'Next steps for the project',
-      template: (title) => {
-        return `# ${title}`;
-      },
-    },
+    // {
+    //   type: 'text',
+    //   name: 'bugs',
+    //   prompt: 'A despription of any known bugs',
+    //   template: (value) => {
+    //     return `# ${value}`;
+    //   },
+    // },
+    // {
+    //   type: 'text',
+    //   name: 'nextSteps',
+    //   prompt: 'Next steps for the project',
+    //   template: (value) => {
+    //     return `# ${value}`;
+    //   },
+    // },
     // {
     //   type: 'list',
     //   name: 'license',
