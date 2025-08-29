0d771c7a6055c01930e0cb04465c5a378cc33894
Ray Luna <leon.luna.ray@gmail.com>
2025-08-28 22:38:42 -0700

    nuxt 3 refactor

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-09-19",
-  nitro: {
-    preset: "cloudflare_module",
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true
-    }
-  },
+    compatibilityDate: "2024-09-19",
+    nitro: {
+      preset: "cloudflare_module",
+      cloudflare: {
+        deployConfig: true,
+        nodeCompat: true
+      }
+    },
   devtools: { enabled: true },
   css: ['~/assets/styles/main.css'],
   modules: [
