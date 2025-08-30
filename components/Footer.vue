0e0779b13eb12ce6f67f8cd5e7acc396fb9cdcae
Ray Luna <leon.luna.ray@gmail.com>
2025-08-29 23:44:54 -0700

    footer

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="fixed bottom-0 w-full">
+        <div class="container flex justify-center items-center py-[0.85rem] ">
+            <p class="text-[1rem] font-[300]">&copy; {{ year }} <a href="https://www.rayluna.dev" target="_blank"
+                    rel="noopener noreferrer" class="underline text-[1rem] font-[300]">rayluna.dev</a>
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+const year = new Date().getFullYear();
+</script>
