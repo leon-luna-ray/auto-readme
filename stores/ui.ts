0e92b71524917f0bb0c115e8e8e361604ccf9a08
Ray Luna <leon.luna.ray@gmail.com>
2025-09-02 20:30:19 -0700

    add user settings

--- stores/ui.ts
+++ stores/ui.ts
@@ -1,17 +1,22 @@
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useStorage } from '@vueuse/core';
 
 import { defineStore } from 'pinia';
 
 // Global Store
 export const useUiStore = defineStore('ui', () => {
+
+  const userSettings = useStorage('auto-readme-settings', {
+    theme: 'hacker',
+  });
+
   // State
   const indicator = '>>>>>> ';
-  const theme = ref<string>('hacker');
+  const theme = computed(() => userSettings.value.theme);
 
   // Methods
   const setTheme = (value: string) => {
-    theme.value = value;
+    userSettings.value.theme = value;
   };
 
   return {
